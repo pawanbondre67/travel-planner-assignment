a19511fab66c90f9968e1266b45dcd8a08f86b74
pawanbondre67 <bondrepavan623@gmail.com>
2024-10-13 03:08:58 +0530

    project uploaded

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA4U3s5Llgws_0tra0He-B_rY_2wx4I_qc",
-    authDomain: "assignment-ed2cf.firebaseapp.com",
-    projectId: "assignment-ed2cf",
-    storageBucket: "assignment-ed2cf.appspot.com",
-    messagingSenderId: "841094638260",
-    appId: "1:841094638260:web:56a05126898910e2a7c5e1"
-  };
-  
+  apiKey: "AIzaSyB3BjusRNWvqgoWZU_eXKMU2yi3QWN1IfY",
+  authDomain: "assignment-49950.firebaseapp.com",
+  projectId: "assignment-49950",
+  storageBucket: "assignment-49950.appspot.com",
+  messagingSenderId: "631775149310",
+  appId: "1:631775149310:web:5f26c7482db83f2481f9af",
+  measurementId: "G-SG14MWB7ZQ"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
